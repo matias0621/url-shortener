e6ec54c21d902df86869e145017dfb06d937873b
matias0621 <mattiascorrea@outlook.com>
2024-03-26 22:56:44 -0300

    asd

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,7 @@
 import { Inter } from "next/font/google";
 import { useRef, useState } from "react";
 
-
 const inter = Inter({ subsets: ["latin"] });
-
 
 export default function Home() {
   const inputRef = useRef();
@@ -14,45 +12,41 @@ export default function Home() {
     e.preventDefault();
     const url = inputRef.current.value;
     //TODO: Peticion al API
-    fetch('/api/shortUrl',{
-      method: 'POST',
-      headers:{
-        'Content-Type': 'application/json'
+    fetch("/api/shortUrl", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({url})
-    }).then (res => res.json())
-    .then((data) => {
-      setShortURL(data.shortUrl); 
-      setUrlArray(prevUrl => [...prevUrl, data.shortUrl]);
+      body: JSON.stringify({ url }),
     })
-  }
+      .then((res) => res.json())
+      .then((data) => {
+        setShortURL(data.shortUrl);
+        setUrlArray((prevUrl) => [...prevUrl, data.shortUrl]);
+      });
+  };
 
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 `}
-    >
-     <h1>URL Shortener</h1>
-     <p>Aorta tus URLs aqui</p>
+    <main className="bg-slate-900 w-screen h-screen flex flex-col justify-center items-center">
+      <h1 className="text-center text-blue-500 text-4xl">URL Shortener</h1>
 
-     <div>
-      <form action="" onSubmit={handleSubmit} className="flex flex-col">
-        <input ref={inputRef} type="text" className="text-black" />
-        <button>Acorta</button>
-        <span className="text-white">
-          {shortURL}
-        </span>
-      </form>
 
-      <div>
-        {
-          urlArray.map((url, index) => (
+      <div className="pl-4">
+        <p className="text-blue-500">Acorta tus URLs aqui</p>
+        <form onSubmit={handleSubmit} className="flex flex-col">
+          <input ref={inputRef} type="text" className="text-black w-40" />
+          <button>Acorta</button>
+          <span className="text-white">{shortURL}</span>
+        </form>
+
+        <div>
+          {urlArray.map((url, index) => (
             <li key={index}>
               <a href={`/${url}`}> {url} </a>
             </li>
-          ))
-        }
+          ))}
+        </div>
       </div>
-     </div>
     </main>
   );
 }

33c4ebb1f4219d82333717a4666684589c5d6870
matias0621 <mattiascorrea@outlook.com>
2024-03-28 15:45:47 -0300

    asd

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -27,22 +27,23 @@ export default function Home() {
   };
 
   return (
-    <main className="bg-slate-900 w-screen h-screen flex flex-col justify-center items-center">
-      <h1 className="text-center text-blue-500 text-4xl">URL Shortener</h1>
+    <main className="bg-slate-900 w-screen h-screen">
+      <h1 className="text-center text-blue-800 text-4xl pt-36 pb-10 font-sans">URL Shortener</h1>
 
 
-      <div className="pl-4">
-        <p className="text-blue-500">Acorta tus URLs aqui</p>
-        <form onSubmit={handleSubmit} className="flex flex-col">
-          <input ref={inputRef} type="text" className="text-black w-40" />
-          <button>Acorta</button>
-          <span className="text-white">{shortURL}</span>
+      <div>
+        <form onSubmit={handleSubmit} className="flex flex-col justify-center items-center">
+          <input ref={inputRef} type="text" placeholder="Ingrese enlace" 
+          className="
+          text-black w-80 h-12 px-2 border rounded-md
+          "/>
+          <button className="bg-red-950 w-80 mt-2 h-12 rounded-md text-xl">Acorta</button>
         </form>
 
         <div>
           {urlArray.map((url, index) => (
-            <li key={index}>
-              <a href={`/${url}`}> {url} </a>
+            <li key={index} className="list-none text-center mt-10">
+              <a href={`/${url}`} className="text-3xl"> {url} </a>
             </li>
           ))}
         </div>

85c38e9a66c4e2d2f9a4e200c9471fadb6837303
matias0621 <mattiascorrea@outlook.com>
2024-04-02 13:23:02 -0300

    asd

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import { useRef, useState } from "react";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,27 +28,51 @@ export default function Home() {
   };
 
   return (
-    <main className="bg-slate-900 w-screen h-screen">
-      <h1 className="text-center text-blue-800 text-4xl pt-36 pb-10 font-sans">URL Shortener</h1>
-
-
-      <div>
-        <form onSubmit={handleSubmit} className="flex flex-col justify-center items-center">
-          <input ref={inputRef} type="text" placeholder="Ingrese enlace" 
-          className="
-          text-black w-80 h-12 px-2 border rounded-md
-          "/>
-          <button className="bg-red-950 w-80 mt-2 h-12 rounded-md text-xl">Acorta</button>
-        </form>
-
-        <div>
-          {urlArray.map((url, index) => (
-            <li key={index} className="list-none text-center mt-10">
-              <a href={`/${url}`} className="text-3xl"> {url} </a>
-            </li>
-          ))}
-        </div>
+    <>
+      <div className="fixed top-0 -z-10 h-full w-full bg-slate-900">
+        <div className="absolute inset-0 -z-9 h-full w-full bg-[#141414] bg-[radial-gradient(#433526_1px,transparent_1px)] [background-size:16px_16px]"></div>
+        <div className="shadow-orange-500 absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-primary-500/40 opacity-50 blur-[80px]"></div>
       </div>
-    </main>
+      <main class="px-8 py-10 ">
+        <div class="flex w-full justify-between text-slate-100">
+          <h1 class="text-4xl font-bold">Shortener URL</h1>
+        </div>
+        <div class="w-full flex flex-col gap-12 md:gap-6 lg:flex-row-reverse justify-center items-center h-[calc(100vh-7.5rem)]">
+          <div class="flex flex-col gap-3 items-center md:items-start">
+            <h1 class="text-center md:text-left text-4xl md:max-w-[15ch] md:text-7xl font font-extrabold text-slate-200">
+              Acorta tus enlaces
+            </h1>
+            <form
+              onSubmit={handleSubmit}
+              id="myForm"
+              class="flex flex-col  w-full mt-6 max-w-[760px] justify-center text-xl mx-auto px-6 py-5 bg-gray-700/60 shadow-xl rounded-md"
+            >
+              <div class="flex flex-col md:flex-row gap-3">
+                <input
+                  class="w-full bg-white text-gray-900 text-base sm:text-lg 2xl:text-xl  px-5 py-2.5 rounded-md outline-none focus:outline-2 focus:outline-primary-500 transition-all"
+                  type="url"
+                  placeholder="Ponga el link aqui"
+                  required=""
+                  ref={inputRef}
+                />
+                <button
+                  type="submit"
+                  class="bg-yellow-200 hover:bg-orange-400 text-slate-950 px-5 py-2.5 capitalize rounded-md transition-colors"
+                >
+                  Acorta!
+                </button>
+              </div>
+              <div class="flex flex-col items-center justify-between">
+                {urlArray.map((url, index) => (
+                  <li key={index} className="list-none text-center mt-10">
+                    <a href={`/${url}`} className="text-3xl"> {url} </a>
+                  </li>
+                ))}
+              </div>
+            </form>
+          </div>
+        </div>
+      </main>
+    </>
   );
 }

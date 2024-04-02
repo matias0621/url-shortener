cd7747293555f8b7b1a37cb311e442cd63d8c381
matias0621 <mattiascorrea@outlook.com>
2024-04-02 13:44:33 -0300

    asd

--- src/pages/api/shortUrl.js
+++ src/pages/api/shortUrl.js
@@ -5,19 +5,23 @@ const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
   const { url } = req.body;
-  const shortUrl = Math.random().toString(36).substring(2, 5);
-  let urlArray = [];
 
   try {
-    const data = await prisma.link.create({
+    // Generar una URL corta única (puedes implementar lógica más robusta aquí)
+    const shortUrl = Math.random().toString(36).substring(2, 5);
+
+    // Crear una nueva entrada en la base de datos
+    await prisma.link.create({
       data: { url, shortUrl },
-    })
+    });
 
-    urlArray.push(data.shortUrl);
+    // Recuperar todas las URLs cortas almacenadas en la base de datos
+    const allLinks = await prisma.link.findMany();
+    const urlArray = allLinks.map(link => link.shortUrl);
 
-    return res.status(200).send(data, urlArray);
+    // Enviar todas las URLs cortas al frontend
+    return res.status(200).send(urlArray);
   } catch (error) {
-    return res.status(500).send({ error })
+    return res.status(500).send({ error });
   }
-
 }
